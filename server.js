c9e7391c8f5f3af7529f2600518f10dd1c9c16d4
Senychak Bohdan <bsenk77@gmail.com>
2026-05-05 11:08:47 +0300

    prev version

--- server.js
+++ server.js
@@ -26,9 +26,112 @@ const httpServer = createServer(async (req, res) => {
 
 const io = new Server(httpServer);
 
+let players = {};
+let ball = { x: 300, y: 200, vx: 4, vy: 3 };
+const sides = ["left", "right"];
+let score = { left: 0, right: 0 };
+let isPlaying = false;
+
 io.on("connection", (socket) => {
-    console.log("Player connected:", socket.id);
+    console.log(
+        chalk.yellow("\nSocket:"),
+        chalk.blue("Player connected id:"),
+        chalk.green(socket.id),
+    );
+
+ if (Object.keys(players).length < 2) {
+        const takenSides = Object.values(players).map(p => p.side);
+        const freeSide = sides.find(s => !takenSides.includes(s));
+
+        players[socket.id] = {
+            y: 150,
+            side: freeSide
+        };
+    }
+
+    socket.emit("init", {
+        id: socket.id,
+        players
+    });
+
+    socket.on("move", (y) => {
+        if (players[socket.id]) {
+            players[socket.id].y = y;
+        }
+    });
+
+    socket.on("start", () => {
+    if (Object.keys(players).length === 2) {
+        isPlaying = true;
+
+        // ресет м’яча
+        ball.x = 300;
+        ball.y = 200;
+        ball.vx = 4;
+        ball.vy = 3;
+    }
 });
+
+    socket.on("disconnect", () => {
+        delete players[socket.id];
+    });
+});
+
+setInterval(() => {
+    if (!isPlaying) {
+        io.emit("state", { players, ball, score, isPlaying });
+        return;
+    }
+
+    ball.x += ball.vx;
+    ball.y += ball.vy;
+
+    // стіни
+    if (ball.y < 0 || ball.y > 400) {
+        ball.vy *= -1;
+    }
+
+    // зіткнення
+    for (let id in players) {
+        let p = players[id];
+
+        if (p.side === "left" && ball.x < 20) {
+            if (ball.y > p.y && ball.y < p.y + 100) {
+                ball.vx *= -1;
+            }
+        }
+
+        if (p.side === "right" && ball.x > 580) {
+            if (ball.y > p.y && ball.y < p.y + 100) {
+                ball.vx *= -1;
+            }
+        }
+    }
+
+    // ГОЛ
+    if (ball.x < 0) {
+        score.right++;
+        resetBall();
+    }
+
+    if (ball.x > 600) {
+        score.left++;
+        resetBall();
+    }
+
+    io.emit("state", { players, ball, score, isPlaying });
+
+}, 1000 / 60);
+
+function resetBall() {
+    isPlaying = false;
+
+    ball.x = 300;
+    ball.y = 200;
+
+    ball.vx = (Math.random() > 0.5 ? 4 : -4);
+    ball.vy = 3;
+}
 
 httpServer.listen(3000, () => {
     console.log("Server running on 3000");
--- static/game.js
+++ static/game.js
@@ -1,1 +1,74 @@
 const socket = io();
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+const startBtn = document.getElementById("startBtn");
+
+startBtn.addEventListener("click", () => {
+    socket.emit("start");
+});
+
+let myId = null;
+let players = {};
+let ball = {};
+let score = { left: 0, right: 0 };
+let isPlaying = false;
+
+socket.on("state", (state) => {
+    players = state.players;
+    ball = state.ball;
+    score = state.score;
+    isPlaying = state.isPlaying;
+
+    draw();
+});
+
+socket.on("init", (data) => {
+    myId = data.id;
+    players = data.players;
+});
+
+socket.on("state", (state) => {
+    players = state.players;
+    ball = state.ball;
+    draw();
+});
+
+// керування мишкою
+canvas.addEventListener("mousemove", (e) => {
+    const rect = canvas.getBoundingClientRect();
+    const y = e.clientY - rect.top;
+
+    socket.emit("move", y);
+});
+
+function draw() {
+    ctx.clearRect(0, 0, 600, 400);
+
+    // гравці
+    for (let id in players) {
+        let p = players[id];
+
+        ctx.fillRect(
+            p.side === "left" ? 10 : 580,
+            p.y,
+            10,
+            100
+        );
+    }
+
+    // м'яч
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, 8, 0, Math.PI * 2);
+    ctx.fill();
+
+    // рахунок
+    ctx.font = "30px Arial";
+    ctx.fillText(score.left, 200, 50);
+    ctx.fillText(score.right, 380, 50);
+
+    // статус
+    if (!isPlaying) {
+        ctx.font = "20px Arial";
+        ctx.fillText("Press START", 230, 200);
+    }
+}
